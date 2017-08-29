9886b8ddad24c2f100e7febbfb515ae98f87e543
Manuel Paniagua <manuel.paniagua84@gmail.com>
2017-08-29 00:12:49 +0000

    Add photos to menu items

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 //... Create your module
 var app = angular.module("MenuApp", []);
+
+$(document).ready(function(){
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+});
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,17 +5,20 @@ app.controller('MainController', ['$scope', function($scope) {
     {
       name: 'Caprese',
       description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
-      price: 4.95
+      price: 4.95,
+      image: 'img/caprese.jpg'
     },
     {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
-      price: 3.95
+      price: 3.95,
+      image: 'img/mozzarella.jpg'
     },
     {
       name: 'Bruschetta',
       description: "Grilled bread, garlic, tomatoes, olive oil.",
-      price: 4.95
+      price: 4.95,
+      image: 'img/bruschetta.jpg'
     }
   ];
   
@@ -23,17 +26,20 @@ app.controller('MainController', ['$scope', function($scope) {
     {
       name: 'Mushroom Trio',
       description: "Cremini mushrooms, Enoki mushrooms, Shiitake mushrooms, spinach, marinara sauce, mozzarella.",
-      price: 8.95
+      price: 8.95,
+      image: 'img/mushroom.jpg'
     },
     {
       name: 'Burrata',
       description: "Burrata, tomato, caramelized onion, arugula, hazelnuts, pesto.",
-      price: 7.95
+      price: 7.95,
+      image: 'img/burrata.jpg'
     },
     {
       name: 'Pepperoni',
       description: "Pepperoni, marinara sauce, mozzarella, fresh oregano.",
-      price: 6.95
+      price: 6.95,
+      image: 'img/peppizza.jpg'
     }
   ];
   
@@ -41,17 +47,20 @@ app.controller('MainController', ['$scope', function($scope) {
     {
       name: 'Breadsticks',
       description: "Served with marinara sauce.",
-      price: 4.95
+      price: 4.95,
+      image: 'img/breadsticks.jpg'
     },
     {
       name: 'Soup of the Day',
       description: "Take a chance.",
-      price: 4.95
+      price: 4.95,
+      image: 'img/soup.jpg'
     },
     {
       name: 'Salad',
       description: "Field greens, balsmaic vinaigrette, asiago.",
-      price: 4.95
+      price: 4.95,
+      image: 'img/salad.jpg'
     }
   ];
 
